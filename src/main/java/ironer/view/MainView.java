787563e6b10e48b425421bcecf932ad6837efa49
Janko <jankoristic@proton.me>
2025-07-22 11:10:35 +0200

    table view

--- src/main/java/ironer/view/MainView.java
+++ src/main/java/ironer/view/MainView.java
@@ -3,20 +3,31 @@ package ironer.view;
 import ironer.model.Iron;
 import ironer.model.enums.IronShape;
 import ironer.model.enums.IronType;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MainView extends Stage {
     private VBox mainBox;
     private List<Iron> ironList;
+    private ObservableList<Iron> ironObservableList;
     /// info row
     private Label identifierLabel = new Label();
     private Label ordererLabel = new Label();
@@ -34,7 +45,8 @@ public class MainView extends Stage {
     private TextField amountTextField = new TextField();
     private Button addButton = new Button("Dodaj");
     /// preview row
-    private ListView<Iron> listView = new ListView<>();
+//    private ListView<Iron> listView = new ListView<>();
+    private TableView tableView = new TableView<>();
     /// final row
     private Label totalG = new Label();
     private TextField totalGTextField = new TextField();
@@ -46,10 +58,13 @@ public class MainView extends Stage {
     public MainView() {
         this.mainBox = new VBox(20);
         this.ironList = new ArrayList<>();
+        this.ironObservableList = FXCollections.observableArrayList();
 
         initInfoRow();
         initAddRow();
         initPreview();
+        initTable();
+        initFinal();
 
         Scene scene = new Scene(mainBox, 850, 700);
         this.setScene(scene);
@@ -112,7 +127,8 @@ public class MainView extends Stage {
             Iron iron = new Iron(IronType.valueOf(this.ironTypeComboBox.getValue()), Double.parseDouble(a1TextField.getText()), Integer.parseInt(this.amountTextField.getText()));
             System.out.println(iron.toString());
             ironList.add(iron);
-            listView.getItems().add(iron);
+            ironObservableList.add(iron);
+//            listView.getItems().add(iron);
         });
         addButton.setDefaultButton(true);
         this.dynamicHBox.getChildren().addAll(a1TextField, amountTextField, addButton);
@@ -131,7 +147,8 @@ public class MainView extends Stage {
                     this.torzionaCheckBox.isSelected()
             );
             ironList.add(iron);
-            listView.getItems().add(iron);
+            ironObservableList.add(iron);
+//            listView.getItems().add(iron);
             System.out.println(iron.toString());
         });
         addButton.setDefaultButton(true);
@@ -140,7 +157,7 @@ public class MainView extends Stage {
     }
 
     private void initPreview(){
-        this.mainBox.getChildren().add(listView);
+//        this.mainBox.getChildren().add(listView);
     }
 
     private void initFinal(){
@@ -151,9 +168,66 @@ public class MainView extends Stage {
         this.totalV.setText("Vezano");
         this.totalVTextField.setText("0.0");
 
-        VBox vBoxG = new VBox(20);
-        vBoxG.setAlignment(Pos.CENTER);
-        VBox vBoxR = new VBox(20);
-        VBox vBoxV = new VBox(20);
+        VBox vBoxG = new VBox(5);
+        vBoxG.setAlignment(Pos.BOTTOM_LEFT);
+        vBoxG.getChildren().addAll(this.totalG, totalGTextField);
+
+        VBox vBoxR = new VBox(5);
+        vBoxR.setAlignment(Pos.BOTTOM_LEFT);
+        vBoxR.getChildren().addAll(this.totalR, totalRTextField);
+
+        VBox vBoxV = new VBox(5);
+        vBoxV.setAlignment(Pos.BOTTOM_LEFT);
+        vBoxV.getChildren().addAll(this.totalV, totalVTextField);
+
+        HBox totalHBox = new HBox(20, vBoxG,vBoxR, vBoxV);
+        totalHBox.setAlignment(Pos.BOTTOM_LEFT);
+        totalHBox.setMaxHeight(30);
+        this.mainBox.getChildren().add(totalHBox);
     }
+
+    private void initTable(){
+        TableColumn drawColumn  = new TableColumn("Skica");
+        drawColumn.setMaxWidth(400);
+        drawColumn.setCellFactory(col -> new TableCell<Void, Void>() {
+            private final Line line = new Line(0, 0, 50, 0); // x1, y1, x2, y2
+
+            {
+                line.setStrokeWidth(2); // Optional: set line thickness
+                line.setStroke(Color.BLACK); // Optional: line color
+            }
+
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    StackPane pane = new StackPane(line);
+                    pane.setAlignment(Pos.CENTER); // center in cell
+                    setGraphic(pane);
+                }
+            }
+        });
+        TableColumn fiColumn  = new TableColumn("fi");
+        fiColumn.setCellValueFactory(new PropertyValueFactory<Iron, IronType>("ironType"));
+        TableColumn lengthColumn = new TableColumn("Duzina");
+        lengthColumn.setMaxWidth(Double.MAX_VALUE);
+        lengthColumn.setMinWidth(300);
+        lengthColumn.setCellValueFactory(new PropertyValueFactory<Iron, Double>("length"));
+        TableColumn amountColumn = new TableColumn("Kolicina");
+        amountColumn.setCellValueFactory(new PropertyValueFactory<Iron, Integer>("amount"));
+        TableColumn weightColumn = new TableColumn("Tezina(kg)");
+        weightColumn.setCellValueFactory(new PropertyValueFactory<Iron, Double>("weight"));
+
+        tableView.setFixedCellSize(80);
+//        tableView.getStylesheets().add(Objects.requireNonNull(getClass().getResource("src/main/resources/style.css")).toExternalForm());
+        tableView.setMaxWidth(Double.MAX_VALUE);
+        tableView.setStyle("-fx-font-size: 18px;");
+        this.tableView.setItems(ironObservableList);
+        this.tableView.getColumns().addAll(drawColumn, fiColumn, lengthColumn, amountColumn, weightColumn);
+
+        this.mainBox.getChildren().addAll(this.tableView);
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,10 @@
 module ironer.ironer {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.kordamp.bootstrapfx.core;
     requires static lombok;
-
-    opens ironer.ironer to javafx.fxml;
+    requires java.desktop;
     exports ironer;
     opens ironer to javafx.fxml;
+    opens ironer.model to javafx.base;
 }
